a6ed6e3978896fbb4166e40adf8fd5dcdca3da6f
JVJ1986 <merchant1.aaatextiles@gmail.com>
2026-05-08 13:04:40 +0530

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://aymxqvlwulylazlqmcmk.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5bW14cWx2d3VseWF6bHFtY21rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgwNjEwNTQsImV4cCI6MjA5MzYzNzA1NH0.Cq62gHljS4YiO3fBcqlNm3c0gbWvhGutvyILs974K2A';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn(
